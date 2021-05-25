742015095826d52c6901e90b12b5401763a0994d
belen <you@example.com>
2021-05-25 00:12:39 -0400

    new

---
+++ Sites/consultas/consulta_tienda_categoria.php
@@ -0,0 +1,36 @@
+<?php include('../templates/header.html');   ?>
+
+<body>
+<?php
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("../config/conexion.php");
+
+    $tipo= $_POST["tipo_producto_elegido"];
+  #Se construye la consulta como un string
+ 	$query = "";
+
+  #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+	$result = $db -> prepare($query);
+	$result -> execute();
+	$tiendas = $result -> fetchAll();
+  ?>
+
+  <table>
+    <tr>
+      <th>Nombre</th>
+      <th>Rut jefe</th>
+      <th>Direccion</th>
+      <th>Comuna</th>
+
+    </tr>
+  
+      <?php
+        // echo $jefes;
+        foreach ($tiendas as $p) {
+          echo "<tr><td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td></tr>";
+      }
+      ?>
+      
+  </table>
+
+<?php include('../templates/footer.html'); ?>
