1aad3ef7163e88229df1620cd3a1dbb15510892c
belen <you@example.com>
2021-05-24 23:19:57 -0400

    new

---
+++ Sites/consultas/consulta_jefe_comuna.php
@@ -0,0 +1,35 @@
+<?php include('../templates/header.html');   ?>
+
+<body>
+<?php
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("../config/conexion.php");
+
+    $comuna = $_POST["comuna_elegida"];
+  #Se construye la consulta como un string
+ 	$query = "SELECT p.nombre, p.rut, p.edad, p.sexo FROM Personal AS p, Tiendas AS t, Trabaja_en as d Direcciones as di WHERE p.rut = t.rut_jefe AND t.did = d.tid AND di.dir_comuna = $comuna;";
+
+  #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+	$result = $db -> prepare($query);
+	$result -> execute();
+	$jefes = $result -> fetchAll();
+  ?>
+
+  <table>
+    <tr>
+      <th>Rut</th>
+      <th>Nombre</th>
+      <th>Edad</th>
+      <th>Sexo</th>
+    </tr>
+  
+      <?php
+        // echo $jefes;
+        foreach ($jefes as $p) {
+          echo "<tr><td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td></tr>";
+      }
+      ?>
+      
+  </table>
+
+<?php include('../templates/footer.html'); ?>
---
+++ Sites/consultas/consulta_usuario_descripcion.php
@@ -0,0 +1,36 @@
+<?php include('../templates/header.html');   ?>
+
+<body>
+<?php
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("../config/conexion.php");
+
+    $descripcion = $_POST["descripcion_producto"];
+  #Se construye la consulta como un string
+ 	$query = "SELECT u.uid, u.rut, u.nombre, u.sexo, u.edad FROM Usuarios AS u, Productos AS p, Compras AS c WHERE u.rut = c.rut_user AND c.pid = p.pid AND p.descripcion LIKE '%$descripcion%' GROUP BY u.nombre, u.uid, u.rut, u.edad, u.sexo, u.did;";
+
+  #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+	$result = $db -> prepare($query);
+	$result -> execute();
+	$usuarios = $result -> fetchAll();
+  ?>
+
+  <table>
+    <tr>
+      <th>ID</th>
+      <th>Rut</th>
+      <th>Nombre</th>
+      <th>Edad</th>
+      <th>Sexo</th>
+    </tr>
+  
+      <?php
+        // echo $jefes;
+        foreach ($usuarios as $p) {
+          echo "<tr><td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td><td>$p[4]</td></tr>";
+      }
+      ?>
+      
+  </table>
+
+<?php include('../templates/footer.html'); ?>
