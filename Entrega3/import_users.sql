6493bf381681601eabd33501589cec058af90439
belen <you@example.com>
2021-07-06 19:19:51 -0400

    import users

--- Entrega3/import_users.sql
+++ Entrega3/import_users.sql
@@ -9,14 +9,14 @@ uidmax int;
 
 BEGIN 
 
-    /* IF 'contrasena' NOT IN (SELECT column_name FROM information_schema.columns WHERE table_name='usuarios') THEN
-        ALTER TABLE usuarios ADD contrasena varchar(20);
-        UPDATE usuarios SET contrasena = LTRIM(STR(RAND()*(100000000-1)+1, 8));
-    END IF; */
     IF 'contrasena' NOT IN (SELECT column_name FROM information_schema.columns WHERE table_name='usuarios') THEN
         ALTER TABLE usuarios ADD contrasena varchar(20);
-        UPDATE usuarios SET contrasena = LTRIM(STR(rand()*(100000000-1)+1, 8);
+        UPDATE usuarios SET contrasena = LTRIM(STR(rand()*(100000000-1)+1, 8));
     END IF;
+    /* IF 'contrasena' NOT IN (SELECT column_name FROM information_schema.columns WHERE table_name='usuarios') THEN
+        ALTER TABLE usuarios ADD contrasena varchar(20);
+        UPDATE usuarios SET contrasena = LTRIM(STR(rand()*(100000000-1)+1, 8);
+    END IF; */
 
     IF urut NOT IN (SELECT rut FROM Usuarios) THEN
         
