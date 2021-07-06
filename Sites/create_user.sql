cf045668b6428a8d292989b0ae053a202ad224dc
belen <you@example.com>
2021-07-05 20:40:36 -0400

    create user

---
+++ Sites/create_user.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE FUNCTION
+
+create_user(unombre varchar(100), urut VARCHAR(25), uedad int, usexo varchar(10), udir_calle varchar(100), udir_comuna varchar(100))
+
+-- declaramos lo que retorna 
+RETURNS BOOLEAN AS $$
+
+
+-- declaramos las variables a utilizar si es que es necesario
+DECLARE
+uidmax int;
+udidmax int;
+
+-- definimos nuestra función
+BEGIN
+
+    IF urut IN (SELECT rut FROM usuarios) THEN
+        RETURN FALSE;
+    END IF;
+
+    SELECT INTO udidmax 
+    SELECT did WHERE dir_calle == udir_calle AND dir_comuna == udir_comuna
+    FROM direcciones;
+
+    SELECT INTO uidmax
+    MAX(uid)
+    FROM usuarios;
+
+    insert into usuarios values(uidmax + 1, nombre, rut, edad, sexo, udidmax);
+    RETURN TRUE;
+
+    END IF;
+        
+
+-- -- finalizamos la definición de la función y declaramos el lenguaje
+END
+$$ language plpgsql
