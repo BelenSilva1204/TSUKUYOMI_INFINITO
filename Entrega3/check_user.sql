728e1a7b5ad6ee35975bf0cd97597ecfbd6a358c
belen <you@example.com>
2021-07-06 04:05:30 -0400

    verify user

--- Entrega3/check_user.sql
+++ Entrega3/check_user.sql
@@ -5,8 +5,10 @@ check_user (urut varchar(25), upass varchar(20))
 RETURNS BOOLEAN AS $$
 
 BEGIN
-    IF urut, upass IN (SELECT rut, contrasena FROM usuarios) THEN
-        RETURN TRUE;
+    IF urut IN (SELECT rut FROM usuarios) THEN
+        IF upass IN (SELECT contrasena FROM usuarios WHERE rut = urut)
+            RETURN TRUE;
+        END IF;
     ELSE
         RETURN FALSE;
     END IF;
