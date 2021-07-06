0cf7c2ac5cc3f4c9a92cdb597fec3b9af4914eec
belen <you@example.com>
2021-07-06 03:31:23 -0400

    import user

--- Entrega3/import_users.sql
+++ Entrega3/import_users.sql
@@ -9,9 +9,13 @@ uidmax int;
 
 BEGIN 
 
-    IF 'contrasena' NOT IN (SELECT column_name FROM information_schema.columns WHERE table_name='usuarios') THEN
+    /* IF 'contrasena' NOT IN (SELECT column_name FROM information_schema.columns WHERE table_name='usuarios') THEN
         ALTER TABLE usuarios ADD contrasena varchar(20);
         UPDATE usuarios SET contrasena = LTRIM(STR(RAND(uid)*(100000000-1)+1, 8));
+    END IF; */
+    IF 'contrasena' NOT IN (SELECT column_name FROM information_schema.columns WHERE table_name='usuarios') THEN
+        ALTER TABLE usuarios ADD contrasena varchar(20);
+        UPDATE usuarios SET contrasena = 1);
     END IF;
 
     IF rut NOT IN (SELECT rut FROM Usuarios) THEN
